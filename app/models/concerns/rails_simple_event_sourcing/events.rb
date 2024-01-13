11396ce9a70ebbb3c6951ef6e442fbb663daacde
Damian Baćkowski <damianbackowski@gmail.com>
2024-01-13 19:10:40 +0100

    Add concern for adding has_many relation to events in models

---
+++ app/models/concerns/rails_simple_event_sourcing/events.rb
@@ -0,0 +1,9 @@
+module RailsSimpleEventSourcing
+  module Events
+    extend ActiveSupport::Concern
+
+    included do
+      has_many :events, class_name: 'RailsSimpleEventSourcing::Event', as: :eventable
+    end
+  end
+end
