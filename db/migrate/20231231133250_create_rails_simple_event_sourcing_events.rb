b5ebd071d506d8190b3795c1b7ebb3c8f33d3b95
Damian Baćkowski <damianbackowski@gmail.com>
2024-08-24 18:57:21 +0200

    Refactor

--- db/migrate/20231231133250_create_rails_simple_event_sourcing_events.rb
+++ db/migrate/20231231133250_create_rails_simple_event_sourcing_events.rb
@@ -3,10 +3,10 @@
 class CreateRailsSimpleEventSourcingEvents < ActiveRecord::Migration[7.1]
   def change
     create_table :rails_simple_event_sourcing_events do |t|
+      t.references :eventable, polymorphic: true
       t.string :type, null: false
       t.string :event_type, null: false
       t.string :aggregate_id
-      t.references :eventable, polymorphic: true
       t.jsonb :payload
       t.jsonb :metadata
 
